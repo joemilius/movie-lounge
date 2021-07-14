86a33110a25db3848be4f9e2c5c13b91c40f57b7
JoeMilius <joemilius@gmail.com>
2021-07-13 18:13:36 -0600

    Like and Dislike Buttons

--- src/Components/WatchListCard.js
+++ src/Components/WatchListCard.js
@@ -1,6 +1,17 @@
-import React from "react";
+import React, {useState} from "react";
 
 function WatchListCard({ img, id, removeMovie }) {
+    const [like, setLike] = useState(false)
+    const [dislike, setDislike] = useState(false)
+
+    function handleLike(){
+        setLike(!like)
+    }
+
+    function handleDislike(){
+        setDislike(!dislike)
+    }
+
   function handleClick(e) {
     fetch(`http://localhost:3000/watchList/${id}`, {
       method: "DELETE",
@@ -11,6 +22,8 @@ function WatchListCard({ img, id, removeMovie }) {
   return (
     <div>
       <img src={img} alt="Movie Poster" />
+      <button onClick={handleLike} style={like ? {background: "green"} : {background: "grey"}}>👍</button>
+      <button onClick={handleDislike} style={dislike ? {background: "red"} : {background: "grey"}}>👎</button>
       <button onClick={handleClick}>Remove Movie</button>
     </div>
   );
